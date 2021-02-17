1648db7f87e60689d57cf4195e1ad0a5fb1c864c
Abdallah Zubaidi <60988633+xbid@users.noreply.github.com>
2021-02-17 15:02:29 +0200

    Update app.js
    some conflicts in the branch that needed to be edited

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 'use strict';
 
 // Sales Data
-'use strict';
-
-// Sales Data
 
 let storeHour = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',];
 let container = document.getElementById('container');
